ad5c18494cf834bbafb5149ca95a23bed87dd518
shinepengwei <shinepengwei@163.com>
2015-06-15 14:53:50 +0800

    顺序节点和并行节点完成，transition和完成状态操作没有考虑

--- BevTest.py
+++ BevTest.py
@@ -5,13 +5,25 @@ import time
 class InputParam(object):
     def __init__(self):
         self.count = 0
+        self.show = True
 
 
-class CON_Reached_TEN(BevNodePrecondition):
+class CON_Reached(BevNodePrecondition):
+    def __init__(self, num):
+        self.num = num
+
     def external_condition(self, input_par):
-        if input_par.count > 100:
+        if input_par.count > self.num:
             return True
         return False
+
+class TerminalRevert(BevNodeTerminal):
+    def __init__(self, parent_node, node_precondition):
+        BevNodeTerminal.__init__(self, parent_node, node_precondition, "ADD")
+
+    def _do_execute(self, input_par, output_par):
+        output_par.show = not input_par.show
+        print("TerminalRevert:" + str(output_par.show))
 
 
 class NODE_Add(BevNodeTerminal):
@@ -33,11 +45,44 @@ class NODE_Minus(BevNodeTerminal):
 
         return NODE_STATUS_EXECUTING
 
+class TerminalAddTo(BevNodeTerminal):
+    def __init__(self, parent_node, node_precondition,num):
+        BevNodeTerminal.__init__(self, parent_node, node_precondition, "NodeAddTo")
+        self.num = num
+
+    def _do_execute(self, input_par, output_par):
+        output_par.count = input_par.count + 1
+        print("NodeAddTo" + str(self.num) + " count:" + str(output_par.count))
+        if output_par.count >= self.num :
+            return NODE_STATUS_FINISH
+        return NODE_STATUS_EXECUTING
+
+class NodeAddTo10(BevNodeTerminal):
+    def __init__(self, parent_node, node_precondition):
+        BevNodeTerminal.__init__(self, parent_node, node_precondition, "NodeAddTo10")
+
+    def _do_execute(self, input_par, output_par):
+        output_par.count = input_par.count + 1
+        print("NodeAddTo10 count:" + str(output_par.count))
+        if output_par.count >= 10 :
+            return NODE_STATUS_FINISH
+        return NODE_STATUS_EXECUTING
+
+class NodeAddTo20(BevNodeTerminal):
+    def __init__(self, parent_node, node_precondition):
+        BevNodeTerminal.__init__(self, parent_node, node_precondition, "NodeAddTo20")
+
+    def _do_execute(self, input_par, output_par):
+        output_par.count = input_par.count + 1
+        print("NodeAddTo20 count:" + str(output_par.count))
+        if output_par.count >= 20:
+            return NODE_STATUS_FINISH
+        return NODE_STATUS_EXECUTING
 
 def test0():
     root = BevNodePrioritySelector(None, None, "Root")
-    NODE_Add(root, BevNodePreconditionNOT(CON_Reached_TEN()))
-    NODE_Minus(root, CON_Reached_TEN())
+    NODE_Add(root, BevNodePreconditionNOT(CON_Reached(10)))
+    NODE_Minus(root, CON_Reached(10))
     input_par = InputParam()
     output_par = InputParam()
     while True:
@@ -46,11 +91,40 @@ def test0():
             root.tick(input_par, output_par)
             input_par.count = output_par.count
 
-# def test1():
-#     root =
+def test1():
+    root = BevNodePrioritySelector(None, None, "Root")
+    seq = BevNodeSequence(root,BevNodePreconditionNOT(CON_Reached(25)), "seq")
+    NodeAddTo10(seq, None)
+    NODE_Add(seq, None)
+    NODE_Add(root, BevNodePreconditionNOT(CON_Reached(40)))
+    input_par = InputParam()
+    output_par = InputParam()
+    while True:
+        time.sleep(0.1)
+        if root.evaluate(input_par):
+            root.tick(input_par, output_par)
+            input_par.count = output_par.count
+
+def test2():
+    root = BevNodePrioritySelector(None, None, "Root")
+    seq = BevNodeSequence(root,BevNodePreconditionNOT(CON_Reached(25)), "seq")
+    NodeAddTo10(seq, None)
+    NODE_Add(seq, None)
+    parallax = BevNodeParallelNode(root, BevNodePreconditionTRUE(), PARALLAX_FINISH_CON_OR, "parallax")
+    TerminalAddTo(parallax, None, 40)
+    TerminalRevert(parallax, None)
+    input_par = InputParam()
+    output_par = InputParam()
+    while True:
+        time.sleep(0.1)
+        if root.evaluate(input_par):
+            root.tick(input_par, output_par)
+            input_par.count = output_par.count
+            input_par.show = output_par.show
+
 
 if __name__ == '__main__':
-    test1()
+    test2()
 
 
 
--- BevTree.py
+++ BevTree.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-#节点运行状态：运行中/运行完成
+# 节点运行状态：运行中/运行完成
 NODE_STATUS_EXECUTING = 0
 NODE_STATUS_FINISH = 1
+
 
 class BevNodePrecondition(object):
     def external_condition(self, input_par):
         pass
+
 
 class BevNodePreconditionTRUE(BevNodePrecondition):
     def external_condition(self, input_par):
@@ -24,6 +26,7 @@ class BevNodePreconditionNOT(BevNodePrecondition):
     def external_condition(self, input_par):
         return not self.lhs.external_condition(input_par)
 
+
 class BevNode(object):
     def __init__(self, parent_node, node_precondition, name=None):
         self.parent_node = parent_node
@@ -40,7 +43,8 @@ class BevNode(object):
         return True
 
     def evaluate(self, input_par):
-        return (self.node_precondition is None or self.node_precondition.external_condition(input_par)) and self._do_evaluate(input_par)
+        return (self.node_precondition is None or self.node_precondition.external_condition(
+            input_par)) and self._do_evaluate(input_par)
 
     def tick(self, input_par, out_par):
         return self._do_tick(input_par, out_par)
@@ -54,13 +58,17 @@ class BevNodeTerminal(BevNode):
         BevNode.__init__(self, parent_node, node_precondition, name)
 
     def _do_tick(self, input_par, out_par):
-        self._do_execute(input_par, out_par)
+        return self._do_execute(input_par, out_par)
 
     def _do_execute(self, input_par, out_par):
         pass
 
 
 class BevNodePrioritySelector(BevNode):
+    '''
+        优先级选择节点
+    '''
+
     def __init__(self, parent_node, node_precondition, name):
         BevNode.__init__(self, parent_node, node_precondition, name)
         self.current_select_index = -1
@@ -76,25 +84,82 @@ class BevNodePrioritySelector(BevNode):
     def _do_tick(self, input_par, out_par):
         return self.child_nodes[self.current_select_index].tick(input_par, out_par)
 
+
+'''
+一般来说，序列节点最后一个子节点应该是可重复的（没有结束），比如攻击分为：[移动过来，挥砍]，挥砍就应该是可重复的。
+如果最后一个子节点结束，有两种处理方式：（``错误``）1.evaluate返回false。2evaluate返回true，但是tick.啥都不做。
+处理方式：相当于重新进入。
+'''
+
+
 class BevNodeSequence(BevNode):
+    '''
+        序列节点
+    '''
+
     def __init__(self, parent_node, node_precondition, name):
         BevNode.__init__(self, parent_node, node_precondition, name)
         self.current_select_index = -1
 
+    # 判断当前执行的节点是否符合条件
     def _do_evaluate(self, input_par):
         if self.current_select_index == -1:
             self.current_select_index = 0
         return self.child_nodes[self.current_select_index].evaluate(input_par)
 
     def _do_tick(self, input_par, out_par):
+        # 重新来过
+        if self.current_select_index >= len(self.child_nodes):
+            self.current_select_index = 0
+
         if self.child_nodes[self.current_select_index].tick(input_par, out_par) == NODE_STATUS_FINISH:
             self.current_select_index += 1
             if self.current_select_index >= len(self.child_nodes):
                 return NODE_STATUS_FINISH
         return self.child_nodes[self.current_select_index].tick(input_par, out_par)
 
-# class BevNodeParallelNode(BevNode):
-#     d
+
+'''
+结束条件：and或者or
+比如逃跑有两个并行子节点【run和嘘声】，只要run结束，那么逃跑就结束。or的关系。
+'''
+PARALLAX_FINISH_CON_OR = 0
+PARALLAX_FINISH_CON_AND = 0
 
 
+class BevNodeParallelNode(BevNode):
+    '''
+        并行节点
+    '''
 
+    def __init__(self, parent_node, node_precondition, finish_condition, name):
+        BevNode.__init__(self, parent_node, node_precondition, name)
+        self.finish_condition = finish_condition
+        self.statuses = []
+
+    def add_child_node(self, node):
+        BevNode.add_child_node(self, node)
+        self.statuses.append(NODE_STATUS_EXECUTING)
+
+    # 依次调用所有的子节点的Evaluate方法，若所有的子节点都返回True，则自身也返回True，否则，返回False
+    def _do_evaluate(self, input_par):
+        for child in self.child_nodes:
+            if not child.evaluate(input_par):
+                return False
+        return True
+
+    def _do_tick(self, input_par, out_par):
+        if self.finish_condition == PARALLAX_FINISH_CON_OR:
+            for child in self.child_nodes:
+                if child.tick(input_par, out_par) == NODE_STATUS_FINISH:
+                    return NODE_STATUS_FINISH
+            return NODE_STATUS_EXECUTING
+        else:
+            ret = NODE_STATUS_FINISH
+            for index, child in enumerate(self.child_nodes):
+                if self.statuses[index] == NODE_STATUS_EXECUTING:
+                    if child.tick(input_par, out_par) == NODE_STATUS_EXECUTING:
+                        ret = NODE_STATUS_EXECUTING
+                    else:
+                        self.statuses[index] = NODE_STATUS_FINISH
+            return ret
